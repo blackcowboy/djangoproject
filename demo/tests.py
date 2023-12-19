11e088790cab2bab07526d2b36aadf1f6f27d65f
niuxiaofu <1311503759@qq.com>
2023-12-19 14:58:59 +0800

    添加静态页面

--- demo/tests.py
+++ demo/tests.py
@@ -1,16 +1,37 @@
-from django.test import TestCase
+import turtle
 
-# Create your tests here.
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import random
+# 创建画布并设置大小
+canvas = turtle.Screen()
+canvas.setup(width=800, height=600)
 
-
-def demo():
-    list = [random.randint(1, 100) for i in range(10)]
-    print(list)
+# 创建海龟对象
+heart_turtle = turtle.Turtle()
 
 
-if __name__ == '__main__':
-    demo()
+# 定义函数来绘制心形
+def draw_heart():
+    heart_turtle.penup()
+    heart_turtle.goto(-150, -200)  # 移动到起始位置
+
+    heart_turtle.pendown()
+    heart_turtle.fillcolor("red")  # 设置填充色为红色
+    heart_turtle.begin_fill()  # 开始填充图形
+
+    for _ in range(2):
+        heart_turtle.forward(300)  # 前进300像素
+
+        heart_turtle.right(90)  # 右转90度
+        heart_turtle.circle(-150, 270)  # 左侧曲线
+
+        heart_turtle.left(140)  # 左转140度
+        heart_turtle.circle(-150, 270)  # 右侧曲线
+
+        heart_turtle.setheading(0)  # 将朝向重新调整为正北
+
+    heart_turtle.end_fill()  # 结束填充图形
+
+
+draw_heart()  # 调用函数绘制心形
+
+# 关闭画布
+canvas.exitonclick()
--- demo/views.py
+++ demo/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 import random
+from datetime import datetime, timedelta
+import time
 import json
+
 
 # Create your views here.
 def index(request):
@@ -12,8 +15,14 @@ def index(request):
 
 def echarsView(request):
     kpidataList = []
+    axis = []
+    startTime = '2023-09-07 00:00:00'
+    axis.append(startTime)
+    for t in range(1441):
+        Time = datetime.strptime(startTime, '%Y-%m-%d %H:%M:%S') + timedelta(minutes=(1 * t))
+        axis.append(str(Time))
     for i in range(2):
-        kpidata = {'axis': [i for i in range(1440)],
+        kpidata = {'axis': axis,
                    'data': [random.randint(1, 100) for i in range(1440)],
                    'total': '交易量' + str(i),
                    'index': i
